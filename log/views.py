071c1646e912bea6aced493fac4886cf9af9cd35
Boffin <boffin626@icloud.com>
2024-06-14 17:18:45 +0200

    'edit entry' view

--- log/views.py
+++ log/views.py
@@ -47,3 +47,20 @@ def new_entry(request, topic_id):
         
     context = {'topic': topic, 'form': form}
     return render(request, 'log/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic 
+    
+    if request.method != "POST":
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('log:topic', topic_id=topic.id)
+    
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'log/edit_entry.html', context)
+    
+    
