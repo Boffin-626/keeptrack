a8a6a53e9b6314520894f9f1b62cc32863f4f1ab
Boffin <boffin626@icloud.com>
2024-06-22 09:39:41 +0200

    modified: log/models (imported User and added owner field)

--- log/models.py
+++ log/models.py
@@ -1,8 +1,10 @@
-from django.db import models
+from django.db import models 
+from django.contrib.auth.models import User 
 
 class Topic(models.Model):
     text = models.CharField(max_length=250)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.text 
